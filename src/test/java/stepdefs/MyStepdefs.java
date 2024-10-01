65cdc43ac8bfb9739da8e0b49572d77832470b98
Atmozo <ashelymozorand@gmail.com>
2024-10-01 14:09:06 +0200

    Gson gson = new Gson();         JsonObject jsonObject = new JsonObject();         map.forEach(jsonObject::addProperty);

--- src/test/java/stepdefs/MyStepdefs.java
+++ src/test/java/stepdefs/MyStepdefs.java
@@ -2,10 +2,13 @@ package stepdefs;
 
 import com.automation.constants.Constants;
 import com.automation.functions.Library;
+import com.automation.pojos.BookingDates;
 import com.automation.pojos.Customer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.BeforeStep;
 import io.cucumber.java.Scenario;
@@ -30,14 +33,15 @@ public class MyStepdefs {
     private Scenario scenario;
     private Response response;
     private RequestSpecification request;
-    private Map<String,String> headers;
+    private Map<String, String> headers;
     private String payload;
     private Gson gson;
+    private Customer customer;
 
 
     @BeforeStep
-    public void startup(Scenario scenario){
-       this.scenario = scenario;
+    public void startup(Scenario scenario) {
+        this.scenario = scenario;
 
     }
 
@@ -45,7 +49,7 @@ public class MyStepdefs {
     @When("I get a author using id as {string} and url {string}")
     public void iGetAAuthorUsingIdAsAndUrl(String arg0, String arg1) {
 
-        RestAssured.baseURI="https://restful-booker.herokuapp.com";
+        RestAssured.baseURI = "https://restful-booker.herokuapp.com";
         headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
 
@@ -61,45 +65,55 @@ public class MyStepdefs {
 
     @And("Validate that the first name is {string}")
     public void validateThatTheFirstNameIs(String arg0) {
-       String firstname = response.getBody().jsonPath().getString("[0].firstName");
-       scenario.log(firstname);
+        String firstname = response.getBody().jsonPath().getString("[0].firstName");
+        scenario.log(firstname);
     }
 
 
     @Then("The system responds with status code {int}")
     public void theSystemRespondsWithStatusCode(int arg0) {
-        assertThat(response.getStatusCode(),is(arg0));
+        assertThat(response.getStatusCode(), is(arg0));
     }
 
     @Given("I have a booking system endpoint {string}")
     public void iHaveABookingSystemEndpoint(String arg0) {
-        scenario.log("Base Uri :"+ Constants.configuration.get("url") + arg0);
+        scenario.log("Base Uri :" + Constants.configuration.get("url") + arg0);
 
     }
 
+
     @When("I enter the following data")
     public void iEnterTheFollowingData(DataTable data) {
-        Map<String,String> map = data.transpose().asMap();
+
+        Map<String, String> map = data.transpose().asMap(String.class, String.class);
+        Gson gson = new Gson();
+        JsonObject jsonObject = new JsonObject();
+        map.forEach(jsonObject::addProperty);  // Add each map entry as a property to the JsonObject
+        customer = gson.fromJson(jsonObject, Customer.class);
+        System.out.println(customer.toString());
+        payload = gson.toJson(customer);
+        System.out.println(payload);
+    }
         //gson =new Gson();
         //JsonElement jsonElement = gson.toJsonTree(map);
 
         //Customer customer = gson.fromJson(jsonElement,Customer.class);
         //scenario.log(customer.toString());
-       // payload = gson.toJson( jsonElement,Customer.class);
+        // payload = gson.toJson( jsonElement,Customer.class);
 
         //scenario.log(payload);
-        ObjectMapper mapper = new ObjectMapper();
-        Customer customer = mapper.convertValue(map, Customer.class);
-        scenario.log(customer.toString());
+//        ObjectMapper mapper = new ObjectMapper();
+//        Customer customer = mapper.convertValue(map, Customer.class);
+//        scenario.log(customer.toString());
 
-       // Customer customer = new Customer(map)
-       // scenario.log(customer.toString());
+        // Customer customer = new Customer(map)
+        // scenario.log(customer.toString());
 
 
         //Constants.dataMap = map;
         //payload = Library.createPayLoad("src/test/resources/templates/createBooking.tpl",map);
         //scenario.log(payload);
-    }
+
 
     @And("I post the data to the system")
     public void iPostTheDataToTheSystem() {
@@ -111,11 +125,11 @@ public class MyStepdefs {
         response = request
                 .headers(headers)
                 .body(payload)
-                .post(  "/booking");
+                .post("/booking");
 
 
         scenario.log(response.getBody().prettyPrint());
-        Customer customer = gson.fromJson(response.getBody().prettyPrint(),Customer.class);
+        Customer customer = gson.fromJson(response.getBody().prettyPrint(), Customer.class);
         scenario.log(customer.getAdditionalneeds());
 
 
@@ -126,41 +140,38 @@ public class MyStepdefs {
         Constants.bookingID = response.getBody().jsonPath().getString("bookingid");
         scenario.log(Constants.bookingID);
 
-        assertThat(Constants.bookingID,is(notNullValue()));
+        assertThat(Constants.bookingID, is(notNullValue()));
     }
 
     @When("I get a booking using bookingid")
     public void iGetABookingUsingBookingid() {
 
-        System.out.println( "/booking/"+Constants.bookingID);
-        scenario.log("/booking/" +Constants.bookingID);
-       // RestAssured.baseURI="https://restful-booker.herokuapp.com";
+        System.out.println("/booking/" + Constants.bookingID);
+        scenario.log("/booking/" + Constants.bookingID);
+        // RestAssured.baseURI="https://restful-booker.herokuapp.com";
         headers = new HashMap<>();
         headers.put("Content-Type", "application/json");
 
         request = RestAssured.given();
         response = request
                 .headers(headers)
-                .get(  "/booking/"+Constants.bookingID);
+                .get("/booking/" + Constants.bookingID);
 
-       // scenario.log(Constants.bookingID);
-        scenario.log("Response Get "+ response.getBody().prettyPrint());
+        // scenario.log(Constants.bookingID);
+        scenario.log("Response Get " + response.getBody().prettyPrint());
 
     }
-
-
-
 
 
     @And("Validate the booking id")
     public void validateTheBookingId() {
 
-        assertThat(response.body().jsonPath().getString("totalprice"),is(Constants.dataMap.get("totalprice")));
+        assertThat(response.body().jsonPath().getString("totalprice"), is(Constants.dataMap.get("totalprice")));
     }
 
     @And("Validate the lastname")
     public void validateTheLastname() {
-        assertThat(response.body().jsonPath().getString("lastname"),is(Constants.dataMap.get("lastname")));
+        assertThat(response.body().jsonPath().getString("lastname"), is(Constants.dataMap.get("lastname")));
 
     }
 }
